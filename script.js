e5283cdd085ca79bea0be6d580c5c2f56fad3fd4
sauravcompose <saurav@compose.co.in>
2023-11-17 18:49:00 +0530

    updated duration

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ import { check, sleep } from "k6";
 export let options = {
     stages: [
         { duration: '5m', target: 5 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
-        { duration: '30m', target: 10 },
-        { duration: '30m', target: 25 }, // stay at 100 users for 30 minutes
+        { duration: '25m', target: 10 },
+        { duration: '25m', target: 25 }, // stay at 100 users for 30 minutes
         { duration: '5m', target: 0 }, // ramp-down to 0 users
     ],
     thresholds: {
