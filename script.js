76cf06d54c34f8a109a36a4d67dff85fc8df6a67
sauravcompose <saurav@compose.co.in>
2023-11-17 16:38:58 +0530

    changed calling api in batches

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export let options = {
-    vus: 10,
-    duration: '100m',
+    stages: [
+        { duration: '5m', target: 5 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+        { duration: '30m', target: 10 },
+        { duration: '30m', target: 25 }, // stay at 100 users for 30 minutes
+        { duration: '5m', target: 0 }, // ramp-down to 0 users
+    ],
     thresholds: {
-        http_req_duration: ['p(95)<500'],
+        http_req_duration: ['p(95)<1000'],
     },
     summaryTrendStats: ['avg', 'p(95)', 'p(99)', 'med', 'min', 'max'],
 };
-
 
 export default function () {
     let response = http.get('https://www.bikeexchange.de/sitemap.xml');
@@ -18,36 +21,18 @@ export default function () {
     let locMatches = xmlData.match(/<loc>(.*?)<\/loc>/g);
 
     let i = 0;
-    const arr = [];
-    let tempArr = [];
 
     locMatches.map(match => {
         const url = match.replace(/<\/?loc>/g, '').replace('https://www.bikeexchange.de/', 'https://d1iwqyn9u48vu3.cloudfront.net/api/cms-app/v1/Page?urlSlug=%2F');
-
-        if (tempArr.length == 10) {
-            arr.push(tempArr);
-            tempArr = [];
-        } else {
-            tempArr.push({
-                method: 'GET',
-                url,
-                params: {
-                    headers: {
-                        'accept': 'application/json',
-                        'vertical': 'de',
-                        'locale': 'de-DE',
-                    },
-                },
-            });
-        }
-    });
-
-    for (let urls of arr) {
-        const res = http.batch(urls);
-        console.log(++i);
-        check(res, {
-            'All responses are 200': (res) => res.every((r) => r.status === 200),
+        let res = http.get(url, {
+            headers: {
+                'accept': 'application/json',
+                'vertical': 'de',
+                'locale': 'de-DE',
+            },
         });
+
+        check(res, { "status was 200": (r) => r.status == 200 });
         sleep(1);
-    }
+    });
 }
